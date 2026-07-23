3366453c4f465564a100ba35fee9b5fd38ff6ce2
willda-s <william.guerreiro2003@gmail.com>
2026-07-23 14:38:42 +0200

    Delete: commentaries

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: willda-s <willda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/18 18:43:11 by williamguer       #+#    #+#             */
-/*   Updated: 2026/07/12 16:09:24 by willda-s         ###   ########.fr       */
+/*   Updated: 2026/07/23 14:32:27 by willda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,13 +88,3 @@ int main()
     delete basePtr;
     return 0;
 }
-
-/*
-Dans le main, Srand est utilisé pour initialiser le générateur de nombres aléatoires avec une valeur basée sur l'heure actuelle. 
-Cela garantit que les résultats seront différents à chaque exécution du programme. 
-
-Dynamic cast est utilisé pour identifier le type réel de l'objet pointé par un pointeur ou une référence de type Base.
-Ici, (void)dynamic_cast<A*>(p) tente de convertir le pointeur p en un pointeur de type A. Si la conversion réussit, cela signifie que l'objet pointé par p est de type A, et "A" est affiché.
-Si la conversion échoue, une exception std::bad_cast est levée, qui est attrapée et ignorée, permettant au programme de continuer à tester les autres types (B et C).
-(void) est utilisé pour indiquer que le résultat de dynamic_cast n'est pas utilisé directement, mais que l'opération est effectuée uniquement pour vérifier le type de l'objet.
-*/
